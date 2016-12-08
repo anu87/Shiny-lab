388553a77b5b293eb0dcc636c71fccf3b1d6148e
Anubhuti Mishra <anubhuti87@gmail.com>
2016-12-08 12:47:47 -0500

    First Commit

---
+++ server.R
@@ -0,0 +1,39 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(rpivotTable)
+library(ggplot2)
+library(dplyr)
+library(xlsx)
+library(reshape)
+library(stringr)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+  
+  tab2<-read.xlsx("/Users/Bhuti 1/Documents/Shiny-lab-data/Lab.xlsx",2)
+  tab2<- tab2[-38,]
+  tab2[,2:22]<- sapply(tab2[,2:22],as.character)
+  tab2[,2:22]<- sapply(tab2[,2:22],as.numeric)
+  df<-melt(tab2)
+  out<- str_split_fixed(df$variable,"_",3)
+  out<-out[,-3]
+  df<- data.frame(df,out)
+ # output$distPlot <- renderPlot({
+  #  x <- df[,3]
+   # hist(x, col = "blue")
+    #ggplot(df,aes(x=X1))+geom_bar(stat = "identity")+facet_grid(X2~.)
+    
+  output$pivot <- renderRpivotTable({
+    rpivotTable(data = df, rows = c("organisationunitname","X2"), cols = "X1", vals = "value",
+                aggregatorName = "List Unique Values",inclusions = list(organisationunitname=list("All OUs")),
+                rendererName = "Lab Table", width = "100%", height = "300px")
+    })
+  })
---
+++ ui.R
@@ -0,0 +1,37 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+
+
+library(shiny)
+library(shinydashboard)
+#dir.create("/Users/Bhuti 1/Documents/Shiny-Workshop copy/www")
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(theme="bootstrap.css",
+  # Application title
+  headerPanel(title="Old Faithful Geyser Data"),
+  sidebarPanel(
+    sidebarMenu(
+      menuItem(tabName = "plot", icon=icon("bar-chart-o"),text = "Histogram",selected = TRUE)
+      
+    )
+  ), 
+  mainPanel(fluidRow(
+    tabItems(
+      tabItem(tabName = "plot", 
+              #slider input for number of bins
+              # box(sliderInput("bins","Number of bins:", min = 1, max = 50,value = 30)),
+              #Plot of the generated distribution
+              box(rpivotTableOutput("pivot",width = 500))
+              #box(plotOutput("distPlot"))
+              
+      )
+      
+    )
+    ))))
+
